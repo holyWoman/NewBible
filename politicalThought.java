81b47b3fa5685a1c9b4d3a061d1438974e027e5c
holyWoman <118401167+holyWoman@users.noreply.github.com>
2022-11-16 17:34:12 -0500

    First Thought

---
+++ politicalThought.java
@@ -0,0 +1,21 @@
+public class politicalThought {
+
+  public static void main(String args[]){
+    
+    boolean isSocialist = false
+    boolean isCapitalist = true;
+    
+    // What is the best ideal system to govern society
+     
+    if (isSocialist) {
+        System.out.println("It would be cool if we had socialism, but not here realistically sadly");
+    }
+    if (isCapitalist) {
+        System.out.println("This is sad, but true");
+    
+    }
+    
+    
+  }
+
+}
